161ef000667bc91f3966582ad8fa80288f804a92
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2015-01-30 16:10:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Listview.2014
+* 「Androidの絵本」の「４章 いろいろなUI - ギャラリー」(p110-111)はちゃんと動かないようなので、同様の機能を「HorizontalScrollView」で実装 
---
+++ src/com/example/scrollview/yo/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.scrollview.yo;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Gallery;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+	private ArrayList<Bitmap> list;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		HorizontalScrollView hsv = new HorizontalScrollView(this);
+		setContentView(hsv);
+		//setContentView(R.layout.activity_main);
+
+		LinearLayout mygallery = new LinearLayout(this);
+		mygallery.setOrientation(LinearLayout.HORIZONTAL);
+		mygallery.setGravity(Gravity.CENTER_VERTICAL);
+		hsv.addView(mygallery);
+		
+		list = new ArrayList<Bitmap>();
+		Resources res = getResources();
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_add));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_delete));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_call));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_camera));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_day));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_directions));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_edit));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_help));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_info_details));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_preferences));
+		list.add(BitmapFactory.decodeResource(res, android.R.drawable.ic_menu_search));
+		
+		for(int i=0; i<list.size(); i++){
+			ImageView iv = new ImageView(this);
+//			iv.setLayoutParams(new LayoutParams(140, 140));
+			iv.setLayoutParams(new LayoutParams(250, 250));
+			iv.setPadding(20, 0, 20, 0);
+			iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			iv.setImageBitmap(list.get(i));
+			mygallery.addView(iv);
+		}
+		
+		
+		
+
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
